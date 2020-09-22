9999d90a9871b31e4a370844c4048fdfb55a1451
ARTushar <1605070@ugrad.cse.buet.ac.bd>
2020-09-22 18:00:38 +0600

    add navigation bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/css/Header.css';
-import { Container } from 'reactstrap';
+import { Collapse, Container, NavbarBrand, Navbar, NavItem, Nav, UncontrolledDropdown, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarToggler } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     return (
         <div classNames="header">
-            <Navbar dark expand="md" className="header__navbar">
-                <Container className="header__container">
-
+            <Navbar color="light" light expand="md" className="header__navbar">
+                <Container className="header__navbar__container">
+                    <NavbarToggler onClick={toggle} />
+                    <NavbarBrand className="mr-auto header__navbar__container__logo" href="/">
+                        Logo
+                    </NavbarBrand>
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav navbar>
+                            <NavItem className="header__navbar__container__navitem">
+                                <NavLink className="nav-link" to="/videos">
+                                    <span className="fa fa-video fa-lg"> Videos</span>
+                                </NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    Categories
+                                </DropdownToggle>
+                                <DropdownMenu>
+                                    <DropdownItem>
+                                        Bags
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        Clothes
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
                 </Container>
             </Navbar>
         </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../assets/Home.css';
+import '../assets/css/Home.css';
 
 function Home() {
     return (
