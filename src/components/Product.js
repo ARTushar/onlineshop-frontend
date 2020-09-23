5d718d3ffea6f14163c49b64faf1c0ee5e4be64c
ARTushar <1605070@ugrad.cse.buet.ac.bd>
2020-09-23 18:00:03 +0600

    add products

---
+++ src/components/Product.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Card, CardBody, CardImg, CardSubtitle,Row, Col, CardTitle } from 'reactstrap';
+import '../assets/css/Product.css';
+import Icon from '@material-ui/core/Icon';
+
+
+function Product({ id, image, title, price, discountPrice, rating, number }) {
+    return (
+        <Col xs={{ size: 5 }} md={{ size: 4 }} xl={{size: 3}} className="product">
+            <Card className="product__card">
+                <CardImg top width="100%" src={image} className="product__card__image"/>
+                <CardBody>
+                    <CardTitle>{title}</CardTitle>
+                    <Row>
+                        <Col sm={{size: 6}} className="product__original__price">
+                            <Icon classnName=""><img className="taka__icon" src="taka.svg" /></Icon>
+                            {price}
+                        </Col>
+                        <Col sm={{size: 6}}>
+                            <Icon classnName=""><img className="taka__icon" src="taka.svg" /></Icon>
+                            {discountPrice}
+                        </Col>
+                        </Row>
+                </CardBody>
+            </Card>
+        </Col>
+    );
+}
+
+export default Product;
