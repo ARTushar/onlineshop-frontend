811688790063118a6ef4f94c730e66142e6515be
raju080 <iqbalhossainraju47@gmail.com>
2020-09-24 11:06:43 +0600

    product rendered dynamically

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,106 +1,47 @@
 import React from 'react';
 import { Row, Col, Container, Jumbotron } from 'reactstrap';
 import '../assets/css/Home.css';
+import { PRODUCTS } from '../shared/products';
 import Product from './Product';
-import Filter from './Filter'
+import Filter from './Filter';
 
 function Home() {
-    return (
-        <div className="home">
-            <hr className="home__divider"/>
-            {/* <Jumbotron className="home__jumbotron">
+	return (
+		<div className='home'>
+			<hr className='home__divider' />
+			<Jumbotron className="home__jumbotron">
                 <Container className="home__jumbotron__container">
                     <Row className="home__jumbotron__container__row">
                         <img src='nipun-banner.jpg' />
                     </Row>
                 </Container>
-            </Jumbotron> */}
-            <Container className="home__container">
-                <Row className="home__main">
-                    {/* <Col md={{size: 2}} className="home__main__filter">
+            </Jumbotron>
+			<Container className='home__container'>
+				<Row className='home__main'>
+					{/* <Col md={{size: 2}} className="home__main__filter">
                     <Filter />
                 </Col> */}
-                    <Col className="">
-                        <Row className="home__main__products">
-                            <Product
-                                id="4903850"
-                                title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
-                                price={199.99}
-                                discountPrice={10}
-                                rating={3}
-                                image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
-                            />
-                            <Product
-                                id="23445930"
-                                title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
-                                price={98.99}
-                                discountPrice={15}
-                                rating={5}
-                                image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
-                            />
-                            <Product
-                                id="3254354325"
-                                title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
-                                price={598.99}
-                                discountPrice={15}
-                                rating={4}
-                                image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
-                            />
-                            <Product
-                                id="4903850"
-                                title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
-                                price={199.99}
-                                discountPrice={10}
-                                rating={3}
-                                image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
-                            />
-                            <Product
-                                id="23445930"
-                                title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
-                                price={98.99}
-                                discountPrice={15}
-                                rating={5}
-                                image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
-                            />
-                            <Product
-                                id="3254354325"
-                                title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
-                                price={598.99}
-                                discountPrice={15}
-                                rating={4}
-                                image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
-                            />
-                            <Product
-                                id="4903850"
-                                title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
-                                price={199.99}
-                                discountPrice={10}
-                                rating={3}
-                                image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
-                            />
-                            <Product
-                                id="23445930"
-                                title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
-                                price={98.99}
-                                discountPrice={15}
-                                rating={5}
-                                image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
-                            />
-                            <Product
-                                id="3254354325"
-                                title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
-                                price={598.99}
-                                discountPrice={15}
-                                rating={4}
-                                image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
-                            />
-                        </Row>
-                    </Col>
-
-                </Row>
-            </Container>
-        </div>
-    )
-};
+					<Col className=''>
+						<Row className='home__main__products'>
+                            {PRODUCTS.map((product) => {
+                                return (
+                                    <Product
+                                        id={product.id}
+                                        title={product.title}
+                                        price={product.price}
+                                        discountPrice={product.price - product.price * product.discount * .01}
+                                        rating={product.rating}
+                                        image={product.image}
+                                    />
+                                );
+                            })}
+							
+						</Row>
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	);
+}
 
 export default Home;
---
+++ src/shared/products.js
@@ -0,0 +1,62 @@
+export const PRODUCTS = [
+	{
+		id: 0,
+		title:
+			'Lamaze Peek-A-Boo Forest, Fun Interactive Baby Book with Inspiring Rhymes and Stories',
+		price: 21.23,
+		rating: 4,
+		discount: 10,
+		image: 'images/products/handBag.jpg',
+	},
+	{
+		id: 1,
+		title: 'Fjallraven, Kanken Classic Backpack for Everyday',
+		price: 24.99,
+		rating: 4,
+		discount: 10,
+		image: 'images/products/bag.jpg',
+	},
+	{
+		id: 2,
+		title: 'The Art of Darksiders Genesis',
+		price: 11.99,
+		rating: 2,
+		discount: 10,
+		image: 'images/products/book.jpg',
+	},
+	{
+		id: 3,
+		title:
+			'PDP 048-082-NA-RD Wired Controller for Xbox One, Xbox One X and Xbox One S, Crimson Red',
+		price: 30.2,
+		rating: 3,
+		discount: 10,
+		image: 'images/products/joystick.jpg',
+	},
+	{
+		id: 4,
+		title: 'Oculus Quest All-in-one VR Gaming Headset – 64GB',
+		price: 249.99,
+		rating: 4,
+		discount: 10,
+		image: 'images/products/VRkit.jpg',
+	},
+	{
+		id: 5,
+		title:
+			'ASUS VivoBook 15 Thin and Light Laptop, 15.6” FHD Display, Intel i3-1005G1 CPU, 8GB RAM, 128GB SSD, Backlit Keyboard, Fingerprint, Windows 10 Home in S Mode, Slate Gray, F512JA-AS34',
+		price: 549.99,
+		rating: 5,
+		discount: 10,
+		image: 'images/products/laptop.jpg',
+	},
+	{
+		id: 6,
+		title: "Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor",
+		price: 199.99,
+		rating: 3,
+		discount: 10,
+		image:
+			'https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg',
+	},
+];
