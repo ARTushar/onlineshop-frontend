0e37d75610bf327d8f408336cf311192962ec796
ARTushar <1605070@ugrad.cse.buet.ac.bd>
2020-11-09 16:45:23 +0600

    remove unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import './app.css';
-import { BrowserRouter, withRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import ConfigureStore from './redux/configureStore';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 import Main from './components/Main';
 import { PersistGate } from 'redux-persist/integration/react';
 import CustomizedSnackbar from './components/CustomizedSnackbar';
 
 const {store, persistor} = ConfigureStore();
-
-/* const mapStateToProps = state => {
-  return {
-    
-  }
-} */
 
 
 function App(props) {
