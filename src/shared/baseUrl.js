8b4ab138626c5baa98cb39cd22424515e976b54c
ARTushar <1605070@ugrad.cse.buet.ac.bd>
2020-12-29 11:18:59 +0600

    change base url

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,2 +1,2 @@
-// export const baseUrl = 'https://nipuns-gallery-bd.el.r.appspot.com';
-export const baseUrl = 'http://localhost:3001'
+export const baseUrl = 'https://nipuns-gallery-bd.el.r.appspot.com';
+// export const baseUrl = 'http://localhost:3001'
